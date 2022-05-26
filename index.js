f54995afdb496a228fd6e5095d0370285de99ce3
Oscine Aves <91872677+OscineAves@users.noreply.github.com>
2022-05-25 23:29:05 -0500

    Add counter, add image-text sync, & cleanup code
    -Added counter to display where the user is in the array of all picked champions.
    -The code that displayed the current champion's name is now a function, allowing for it to be synchronized with the load of the relevant image.
    -Insertion of a champion name into the ddragon image is now a function, removing the nameFixed variable and allowing for cleaner code (especially concerning Fiddlesticks' updated splash art not being within ddragon.)
    -Better declarations of certain variables.
    -Better variable names.

--- index.js
+++ index.js
@@ -1,53 +1,57 @@
-const champions = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Azir", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Kha'Zix", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Quinn", "Rammus", "Rek'Sai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zyra", "Bard", "Ekko", "Tahm Kench", "Kindred", "Illaoi", "Jhin", "Aurelion Sol", "Taliyah", "Kled", "Ivern", "Camille", "Rakan", "Xayah", "Kayn", "Ornn", "Zoe", "Kai'Sa", "Pyke", "Neeko", "Sylas", "Yuumi", "Qiyana", "Senna", "Aphelios", "Sett", "Lillia", "Yone", "Samira", "Seraphine", "Rell", "Viego", "Gwen", "Akshan", "Vex", "Zeri", "Renata Glasc"];
-let pastRandChamp = [];
-randNewChamp(champions[getRandomInt(champions.length)]);
+const ChampionsList = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Azir", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Kha'Zix", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Quinn", "Rammus", "Rek'Sai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zyra", "Bard", "Ekko", "Tahm Kench", "Kindred", "Illaoi", "Jhin", "Aurelion Sol", "Taliyah", "Kled", "Ivern", "Camille", "Rakan", "Xayah", "Kayn", "Ornn", "Zoe", "Kai'Sa", "Pyke", "Neeko", "Sylas", "Yuumi", "Qiyana", "Senna", "Aphelios", "Sett", "Lillia", "Yone", "Samira", "Seraphine", "Rell", "Viego", "Gwen", "Akshan", "Vex", "Zeri", "Renata Glasc"];
+let previousChampions = [];
+let currentChamp = null;
+let championName = null;
+getRandomChampion(ChampionsList[getRandomInt(ChampionsList.length)]);
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
-function randNewChamp() {
-  let randChamp = champions[getRandomInt(champions.length)];
-  pastRandChamp.push(randChamp);
-  currentChamp = pastRandChamp.length - 1;
-  console.log(currentChamp);
-  displayChampion(randChamp);
+function getRandomChampion() {
+  let randomChampion = ChampionsList[getRandomInt(ChampionsList.length)];
+  previousChampions.push(randomChampion);
+  currentChamp = previousChampions.length - 1;
+  championName = randomChampion;
+  displayCurrentChampion();
 }
 
-function displayChampion(name) {
-  console.log(currentChamp);
+function displayCurrentChampion() {
+  let image = document.getElementById("championImage");
   //Modifies Display Names to match what is needed for insertion into Image Links
-  switch (name) {
+  switch (championName) {
     case "Kai'Sa":
     case "Kha'Zix":
     case "Vel'Koz":
     case "Cho'Gath":
     case "LeBlanc":
-      nameFixed = nameFixed.charAt(0) + nameFixed.slice(1).toLowerCase();
+      image.src = insertNameIntoLink(championName.charAt(0) + championName.slice(1).toLowerCase());
       break;
     case "Dr. Mundo":
-      nameFixed = name.replace(". ", "");
+      image.src = insertNameIntoLink(championName.replace(". ", ""));
       break;
     case "Wukong":
-      nameFixed = "MonkeyKing";
+      image.src = insertNameIntoLink("MonkeyKing");
       break;
     case "Renata Glasc":
-      nameFixed = "Renata";
+      image.src = insertNameIntoLink("Renata");
+      break;
+    case "Fiddlesticks":
+      image.src = "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blteafcbfed69c501b5/61e2114879b5bd5a1ef7b2ea/011422_Fiddlesticks_Splash_v1.jpg";
       break;
     default:
-      nameFixed = name.replace(" ", "").replace(`'`, ``);
+      image.src = insertNameIntoLink(championName);
       break;
   }
-  //Gets Image from LoL official site, updates displayed information
-  let image = document.getElementById("championImage");
-  //Sketchy code for Fiddlesticks' new art, which is completely seperate from the database of the rest for some godforsaken reason//
-  if (name == "Fiddlesticks") {
-    image.src = "https://images.contentstack.io/v3/assets/blt731acb42bb3d1659/blteafcbfed69c501b5/61e2114879b5bd5a1ef7b2ea/011422_Fiddlesticks_Splash_v1.jpg";
-    document.getElementById("characterTitle").innerHTML = name;
-  } else {
-    image.src = "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + nameFixed + "_0.jpg";
-    document.getElementById("characterTitle").innerHTML = name;
-  }
+  document.getElementById("currentChamp").innerHTML = currentChamp + 1 + "/" + previousChampions.length;
+}
+
+function insertNameIntoLink(nameToInsert) {
+  return "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + nameToInsert.replace(" ", "").replace(`'`, ``) + "_0.jpg";
+}
+
+function displayChampionName() {
+  document.getElementById("characterTitle").innerHTML = championName;
 }
 
 document.onkeyup = function (event) {
@@ -56,14 +60,16 @@ document.onkeyup = function (event) {
     case "ArrowDown":
       if (currentChamp != 0) {
         currentChamp = currentChamp - 1;
-        displayChampion(pastRandChamp[currentChamp]);
+        championName = previousChampions[currentChamp];
+        displayCurrentChampion();
       }
       break;
     case "ArrowRight":
     case "ArrowUp":
-      if (currentChamp != pastRandChamp.length - 1) {
+      if (currentChamp != previousChampions.length - 1) {
         currentChamp = currentChamp + 1;
-        displayChampion(pastRandChamp[currentChamp]);
+        championName = previousChampions[currentChamp];
+        displayCurrentChampion();
       }
       break;
   }
