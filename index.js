9e69e2a15e63e24cd9e9cee680751511105262af
Oscine Aves <91872677+OscineAves@users.noreply.github.com>
2022-05-30 19:15:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,85 +1,93 @@
-let ChampionsList = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Azir", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Kha'Zix", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Quinn", "Rammus", "Rek'Sai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zyra", "Bard", "Ekko", "Tahm Kench", "Kindred", "Illaoi", "Jhin", "Aurelion Sol", "Taliyah", "Kled", "Ivern", "Camille", "Rakan", "Xayah", "Kayn", "Ornn", "Zoe", "Kai'Sa", "Pyke", "Neeko", "Sylas", "Yuumi", "Qiyana", "Senna", "Aphelios", "Sett", "Lillia", "Yone", "Samira", "Seraphine", "Rell", "Viego", "Gwen", "Akshan", "Vex", "Zeri", "Renata Glasc"];
-let previousChampions = [];
+let championsList = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Azir", "Blitzcrank", "Brand", "Braum", "Caitlyn", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana", "Dr. Mundo", "Draven", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Irelia", "Janna", "Jarvan IV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle", "Kennen", "Kha'Zix", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian", "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami", "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Pantheon", "Poppy", "Quinn", "Rammus", "Rek'Sai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen", "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr", "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong", "Xerath", "Xin Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zyra", "Bard", "Ekko", "Tahm Kench", "Kindred", "Illaoi", "Jhin", "Aurelion Sol", "Taliyah", "Kled", "Ivern", "Camille", "Rakan", "Xayah", "Kayn", "Ornn", "Zoe", "Kai'Sa", "Pyke", "Neeko", "Sylas", "Yuumi", "Qiyana", "Senna", "Aphelios", "Sett", "Lillia", "Yone", "Samira", "Seraphine", "Rell", "Viego", "Gwen", "Akshan", "Vex", "Zeri", "Renata Glasc"];
+let prevChampions = [];
 let currentChamp = null;
-let championName = null;
-getRandomChampion(ChampionsList[getRandomInt(ChampionsList.length)]);
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
 function getRandomChampion() {
-  let randomChampion = ChampionsList[getRandomInt(ChampionsList.length)];
-  previousChampions.push(randomChampion);
-  currentChamp = previousChampions.length - 1;
-  championName = randomChampion;
-  displayCurrentChampion();
+  prevChampions.push(championsList[getRandomInt(159)]);
+  currentChampion = prevChampions.length - 1;
+  return prevChampions.slice(-1);
 }
 
-function displayCurrentChampion() {
+function displayChampion(championName) {
   let image = document.getElementById("championImage");
-  //Modifies Display Names to match what is needed for insertion into Image Links
+  let text = document.getElementById("championName");
+  championName = championName.toString();
   switch (championName) {
     case "Kai'Sa":
     case "Kha'Zix":
     case "Vel'Koz":
     case "Cho'Gath":
     case "LeBlanc":
-      image.src = insertNameIntoLink(championName.charAt(0) + championName.slice(1).toLowerCase());
+      image.src = imageLink(championName.charAt(0) + championName.slice(1).toLowerCase());
       break;
     case "Dr. Mundo":
-      image.src = insertNameIntoLink(championName.replace(". ", ""));
+      image.src = imageLink(championName.replace(". ", ""));
       break;
     case "Wukong":
-      image.src = insertNameIntoLink("MonkeyKing");
+      image.src = imageLink("MonkeyKing");
       break;
     case "Renata Glasc":
-      image.src = insertNameIntoLink("Renata");
+      image.src = imageLink("Renata");
+      text.href = pageLink("renata");
       break;
     case "Fiddlesticks":
       image.src = "https://i.ibb.co/2ty4Xzm/kelly-aleshire-basefiddlesticks-final.jpg";
       break;
     default:
-      image.src = insertNameIntoLink(championName);
+      image.src = imageLink(championName);
+      text.href = pageLink(championName);
       break;
   }
-  document.getElementById("currentChamp").innerHTML = currentChamp + 1 + "/" + previousChampions.length;
-  console.log(document.getElementById("championImage").width * document.getElementById("championImage").height + " " + championName);
+  document.getElementById("championName").innerHTML = championName;
+  document.getElementById("championCounter").innerHTML = currentChampion + 1 + "/" + prevChampions.length;
 }
 
-function insertNameIntoLink(nameToInsert) {
-  return "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + nameToInsert.replace(" ", "").replace(`'`, ``) + "_0.jpg";
+function imageLink(championName) {
+  return "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/" + championName.replace(" ", "").replace(`'`, ``) + "_0.jpg";
 }
 
-function updateText() {
-  document.getElementById("characterTitle").innerHTML = championName;
+function pageLink(championName) {
+  return "https://www.leagueoflegends.com/en-us/champions/" + championName.replace(" ", "-").replace(".", "").replace(`'`, ``).toLowerCase();
+}
+
+function gotoPreviousChampion() {
+  if (currentChampion == 0 && prevChampions.length > 1) {
+    currentChampion = prevChampions.length - 1;
+  } else {
+    --currentChampion;
+  }
+  displayChampion(prevChampions[currentChampion]);
+}
+
+function gotoNextChampion() {
+  if (currentChampion + 1 == prevChampions.length) {
+    currentChampion = 0;
+  } else {
+    ++currentChampion;
+  }
+  displayChampion(prevChampions[currentChampion]);
 }
 
 document.onkeyup = function (event) {
   switch (event.key) {
     case "ArrowLeft":
     case "ArrowDown":
-      if (currentChamp != 0) {
-        currentChamp = currentChamp - 1;
-        championName = previousChampions[currentChamp];
-        displayCurrentChampion();
-      }
+      gotoPreviousChampion();
       break;
     case "ArrowRight":
     case "ArrowUp":
-      if (currentChamp != previousChampions.length - 1) {
-        currentChamp = currentChamp + 1;
-        championName = previousChampions[currentChamp];
-        displayCurrentChampion();
-      }
+      gotoNextChampion();
       break;
   }
 };
 
-/* Attempt to display every champion once. For Debugging.
-
+displayChampion(getRandomChampion());
+/* Displays all the champions in championsList. For Debugging
 for (let i = 0; (i < 159); i++) {
-  championName = ChampionsList[i];
-	displayCurrentChampion();
+	displayChampion(championsList[i]);
 }
 */
